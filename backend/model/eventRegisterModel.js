1ecc818755ee308e3977616f7894ec9c6527b607
Ab1jeet <97900133+Ab1jeet@users.noreply.github.com>
2022-06-12 23:20:20 +0545

    created model for event

--- backend/model/eventRegisterModel.js
+++ backend/model/eventRegisterModel.js
@@ -13,15 +13,20 @@ const registerSchema = new mongoose.Schema({
         type: String,
         require: [true, 'banner image is required'] 
     },
+    speakers:{
+        type: [String],
+        require: [true, 'event can\'t be run without speaker']
+    }
+    ,
     date:{
         type: Date,
         require: true
     },
-    slug: {
-        type: [String],
-        require: true
-    }
+    slug: [String],
 })
 
+registerSchema.pre('save', function (next) {
+    return this.slug = [this.name, this.speakers]  
+})
 const registerModel = mongoose.model('registerModel', registerSchema);
 module.exports = registerModel;

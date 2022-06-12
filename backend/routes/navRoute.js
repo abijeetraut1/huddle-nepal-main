e9ceaffe6be7134581b9e1ee9a78dfa804be888c
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2022-06-12 23:27:55 +0545

    create some changes in route section

--- backend/routes/navRoute.js
+++ backend/routes/navRoute.js
@@ -1,17 +1,19 @@
 const express = require("express");
-const router = express.Router();
 const navController = require("./../controllers/navController");
+const eventRegister = require("../controllers/eventRegisterController");
+const router = express.Router({mergeParams: true});
 
-router
-    .route("/how-we-work")
-    .get(navController.how_we_work);
-    
-router
-    .route("/event-listening")
-    .get(navController.event_listening);
 
-router
-    .route("/contact")
-    .get(navController.contact);
+router.route("/").get(navController.home);
+
+router.route("/how-we-work").get(navController.how_we_work);
+
+router.route("/event-listening").get(navController.event_listening);
+
+router.route("/contact").get(navController.contact);
+
+router.route("/register").post(eventRegister.register);
+
+router.route("/event/:slug").get(navController.getEventData)
 
 module.exports = router;

ffa5b70ea236bbfe7c3031398280ef123afa9439
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2022-06-14 23:32:29 +0545

    Update navController.js

--- backend/controllers/navController.js
+++ backend/controllers/navController.js
@@ -1,4 +1,15 @@
-// note next isn't necessary
+const event = require('./../model/eventRegisterModel');
+
+exports.home = async (req, res, next) => {
+    const events = await event.find();
+
+    res.status(200).json({
+        message: 'we are at the home page',
+        length: event.length,
+        event
+    })
+}
+
 exports.how_we_work = (req, res, next) => {
     res.status(200).json({
         message: 'how we work page'

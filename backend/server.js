4dae70bcc78b1df8a1e5f96cbda85e0a1571c2bf
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2022-06-14 23:29:42 +0545

    updated server.js
    this is done to reduce an error called circular path

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,27 @@
-require("dotenv").config(); // dotenv packaged required and initiliazed on the go
-const app = require("./app");
-const connectMongoDB = require("./database");
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
 
-//MongoDb connection
-connectMongoDB(process.env.MONGODB_URI);
+const app = require('./app');
 
-//Listening on PORT
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running at port , ${PORT}`);
+const database_password = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.MONGO_PASSWORD
+);
+
+mongoose
+  .connect(database_password)
+  .then(() => {
+    console.log('database connection sucessfull');
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+console.log(process.env.NODE_ENV);
+
+const port = 3000 || process.env.port;
+
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
 });
